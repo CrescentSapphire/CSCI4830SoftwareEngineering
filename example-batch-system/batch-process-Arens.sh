f482fd9fafcb59a2784739d513a3218bd0ec28f1
CrescentSapphire <43737503+CrescentSapphire@users.noreply.github.com>
2021-02-21 15:57:20 -0600

    Update batch-process-Arens.sh

--- example-batch-system/batch-process-Arens.sh
+++ example-batch-system/batch-process-Arens.sh
@@ -1,7 +1,7 @@
 #! /bin/sh 
 for f in "$1"/*; do
   if [ -f "$f" ]; then
-    SIZE="$(wc -w "${f}" | cut -d " " f 1)"
+    SIZE="$(wc -w "${f}" | cut -d " " -f 1)"
     echo "Processing $f file..."
     echo "$SIZE"
   fi

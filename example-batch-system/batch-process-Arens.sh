6dc73c2599e5eae9f2a42178aa92f390b6997979
CrescentSapphire <43737503+CrescentSapphire@users.noreply.github.com>
2021-02-21 16:29:45 -0600

    Update batch-process-Arens.sh

--- example-batch-system/batch-process-Arens.sh
+++ example-batch-system/batch-process-Arens.sh
@@ -1,9 +1,11 @@
 #! /bin/sh 
 for f in "$1"/*; do
   if [ -f "$f" ]; then
-    SIZE="$(wc -w "${f}" | cut -d ' ' -f 1)"
+    SIZE="$(du -sh "${f}" | cut -f 1)"
+    WORD="$(wc -w "${f}" | cut -d ' ' -f 1)"
     echo "Processing $f file..."
-    echo "$SIZE"
+    echo "File size: $SIZE"
+    echo "Word Count: $WORD"
   fi
 done
 
